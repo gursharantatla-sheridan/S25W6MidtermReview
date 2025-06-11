e569fddfdda4089073d97ba175ff664e7eff64a4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-06-11 12:44:36 -0400

    clear and close

--- S25W6MidtermReview/MainWindow.xaml.cs
+++ S25W6MidtermReview/MainWindow.xaml.cs
@@ -19,6 +19,32 @@ namespace S25W6MidtermReview
         public MainWindow()
         {
             InitializeComponent();
+
+            rdoHourly.IsChecked = true;
+        }
+
+        private void rdoHourly_Checked(object sender, RoutedEventArgs e)
+        {
+            lblInput2.Content = "Hours Worked:";
+            lblInput3.Content = "Hourly Wage:";
+        }
+
+        private void rdoCommission_Checked(object sender, RoutedEventArgs e)
+        {
+            lblInput2.Content = "Gross Sales:";
+            lblInput3.Content = "Commission Rate:";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            rdoHourly.IsChecked = true;
+            txtName.Text = txtInput2.Text = txtInput3.Text = "";
+            txtGrossEarnings.Text = txtTax.Text = txtNetEarnings.Text = "";
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
